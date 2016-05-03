ff2229cd963cec039be95b4d6395bcd285b30c2c
queni <queni@queni-PC.mshome.net>
2016-05-03 16:54:09 +0200

    tribute.php

---
+++ tribute.php
@@ -0,0 +1,242 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <!--<meta http-equiv="refresh" content="2">-->
+        <meta charset="utf-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">    
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <title> tribute </title>
+        <style>
+            body {
+                margin-top:50px;
+            }
+            /*.container *{*/
+            /*.well * {*/
+            .jumbotron * {
+/*                margin-left: auto;
+                margin-right: auto;
+                max-width:600px;*/
+            }
+            div.noLimit {
+/*                margin-left: auto;
+                margin-right: auto;*/
+                /*width:100% !important;*/
+            }
+            h1.lead {
+                font-size: 400%;
+            }
+/*            .thumbnail {
+                max-width: 800px;
+            }*/
+/*            .img-thumbnail {
+                max-width: 800px;
+            }*/
+        </style>
+    </head>
+    <body>
+        <!--<div>-->
+        <!--<div class="container-fluid">-->
+        <div class="container">
+            <div class="jumbotron">
+                <h1 class="lead text-center">Homer Jay Simpson</h1>
+                <h2 class="text-center"><em>The man who did everything</em></h2>    
+<!--                <img src="img/882ecb483ff68ad7863524a7bd5a360b.jpg" alt="" class="img-responsive"/>
+                <img src="img/Homer_Simps   on_2006.png" alt="" class="img-responsive"/>
+                <img src="img/homer-simpson-08.jpg" alt="" class="img-responsive"/>-->
+                <!--<div class="thumbnail text-center">-->
+                <div class="text-center noLimit">
+                    <!--<div class="img-thumbnail img-responsive text-center">-->
+                    <div class="thumbnail  ">
+                        <!--<img src="img/homer_2008_v2F_hires2.jpg" alt="" class="img-responsive"/>-->
+                        <!--<img src="img/homer_2008_v2F_hires2.jpg" alt=""/>-->
+                        <img src="img/homer-simpson-the-simpsons-cartoons-1920x1080-wallpaper478364.jpg" alt="" class="img-responsive"/>
+                        <div class="caption"><a href="http://randomwallpapers.net/homer-simpson-the-simpsons-cartoons-1920x1080-wallpaper478364.jpg"> Homer J. Simpson </a></div>
+                    </div>
+                </div>
+                <div class="row">
+<!--                    <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+                    <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">-->
+                    <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+            <ul>
+                <li>Account Executive ("The Man in the Blue Flannel Pants")</li><li>
+                Acrobat ("Goo Goo Gai Pan")</li><li>
+                Actor ("Homer the Whopper", "A Midsummer's Nice Dream", "Ned 'N Edna's Blend")</li><li>
+                Army private (2 times)</li><li>
+                Agent (3 times)</li><li>
+                Ambulance driver [8]</li><li>
+                Amateur-Division Professional Arm Wrestler [9]</li><li>
+                Asking man at products demonstration ("Springfield Up")</li><li>
+                Astronaut for NASA [10]</li><li>
+                Assassin ("Treehouse of Horror XVIII")</li><li>
+                Attack-dog trainer for the Pitiless Pup Attack Dog School [11]</li><li>
+                Baby Proofer for his own baby proofing company "Wee Care" ("Bye Bye Nerdie")</li><li>
+                Bartender</li><li>
+                Beef Jerky Manufacturer ("Smoke on the Daughter")</li><li>
+                Blackjack dealer of Monty Burns Casino ("$pringfield")</li><li>
+                Bodyguard of Mayor Quimby ("Mayored to the Mob")</li><li>
+                Bootlegger</li><li>
+                The Homer They Fall (Promo Picture) 2</li><li>
+                Bowling alley employee, or "Pin Monkey", of Barney's Bowlarama ("And Maggie Makes Three")</li><li>
+                Bounty Hunter ("Sex, Pies, and Idiot Scrapes")</li><li>
+                Boxer ("The Homer They Fall")</li><li>
+                Butler ("The Frying Game")</li><li>
+                Bum Wrestler</li><li>
+                Bon - Ah</li><li>
+                Candle maker of Ye Olde Candlemaker Shoppe, Olde Springfield Towne ("I Married Marge")</li><li>
+                Car designer for Powell Motors ("Oh Brother, Where Art Thou?")</li><li>
+                Caricaturist of open coffins ("Springfield Up")</li><li>
+                Carny ("Bart Carny")</li><li>
+                CEO of the Springfield Nuclear Power Plant ("C.E. D'oh")</li><li>
+                Chauffeur for Classy Joe's ("Homer vs. Patty and Selma")</li><li>
+                Springfield Chief of Police ("Poppa's Got a Brand New Badge")</li><li>
+                Child Caretaker of Uncle Homer's Daycare Center ("Children of a Lesser Clod")</li><li>
+                Choreographer for the Super Bowl Halftime Show ("Homer and Ned's Hail Mary Pass")</li><li>
+                Homer Car</li><li>
+                Chiropractor ("Pokey Mom")</li><li>
+                Coach for Bart's Little League American football team ("Bart Star")</li><li>
+                Cook at a diner when fleeing from a Florida sheriff ("Kill the Alligator and Run")</li><li>
+                Con artist/Grifter with son Bart ("The Great Money Caper")</li><li>
+                Conceptual artist ("Mom and Pop Art")</li><li>
+                Convenience store night-clerk for the Kwik-E-Mart ("Lisa's Pony")</li><li>
+                Curling player ("Boy Meets Curl")</li><li>
+                Detective ("King-Size Homer" not shown, but mentioned by Marge)</li><li>
+                Drug Smuggler when Burns cancelled the prescription drug program ("Midnight Rx")</li><li>
+                Door-to-door knife salesman for Slash-Co. ("I Married Marge")</li><li>
+                Door-to-door sugar salesman ("Lisa's Rival")</li><li>
+                Door-to-door spring salesman ("The Old Man and the "C" Student")</li><li>
+                Duffman ("The Duffman Cometh!)</li><li>
+                Deacon of the Church ("Pulpit Friction")</li><li>
+                Employee at Gulp 'n' Blow Drive-through ("I Married Marge")</li><li>
+                Executive of Globex Corporation, Cypress Creek ("You Only Move Twice")</li><li>
+                Executive Vice President of Power Plant ("Weekend at Burnsie's")</li><li>
+                Farmer ("E-I-E-I-D'oh")</li><li>
+                Film critic ("A Star is Burns")</li><li>
+                Film producer ("Beyond Blunderdome")</li><li>
+                Fireman (volunteer) ("Crook and Ladder")</li><li>
+                Fish gutter ("Thirty Minutes Over Tokyo")</li><li>
+                Fisherman ("The Wife Aquatic")</li><li>
+                Food critic for The Springfield Shopper ("Guess Who's Coming to Criticize Dinner?")</li><li>
+                Foot Locker employee ("Marge Gamer")</li><li>
+                Fortune cookie writer ("A Hunka Hunka Burns in Love")</li><li>
+                Founder and Junior Vice President Compu-Global-Hyper-Mega-Net ("Das Bus")</li><li>
+                Garbage Commissioner ("Trash of the Titans")</li><li>
+                Grease collector ("Lard of the Dance")</li><li>
+                Grim Reaper ("Treehouse of Horror XIV")</li><li>
+                Guard of Springfield Juvenile Correctional Facility ("The Wandering Juvie")</li><li>
+                Hitman ("Treehouse of Horror XVIII")</li><li>
+                Hairdresser ("Homer Scissorhands")</li><li>
+                Ice Cream Truck Driver ("Ice Cream of Margie (with the Light Blue Hair)")</li><li>
+                Impotency Spokesman for Viag rogaine ("Barting Over")</li><li>
+                Informant for the F.B.I. ("The Trouble with Trillions")</li><li>
+                Internet Service Provider ("Das Bus")</li><li>
+                Inventor ("The Wizard of Evergreen Terrace, Flaming Moe's, The Old Man and the "C" Student")</li><li>
+                Krusty Impersonator ("Homie the Clown")</li><li>
+                Mall Santa Claus ("Simpsons Roasting on an Open Fire")</li><li>
+                Marriage Counselor ("Marge and Homer Turn a Couple Play")</li><li>
+                Mascot, known as "Dancing Homer" ("Dancin' Homer")</li><li>
+                Mattress Salesman ("The Boys of Bummer")</li><li>
+                Manure Salesman ("Springfield Up")</li><li>
+                Mayor of New Springfield ("A Tale of Two Springfields")</li><li>
+                Mayoral candidate ("See Homer Run")</li><li>
+                Mexican wrestler ("Jazzy and the Pussycats")</li><li>
+                Mini-golf assistant for Sir Putt-A-Lot's Merrie Olde Fun Centre ("I Married Marge")</li><li>
+                Missionary ("Missionary: Impossible")</li><li>
+                Mob Boss ("The Mook, the Chef, the Wife and Her Homer")</li><li>
+                Springfield Monorail conductor ("Marge vs. the Monorail")</li><li>
+                Mountain climber ("King of the Hill")</li><li>
+                Musician</li><li>
+                Nuclear Power Plant manager ("Kiss Kiss Bang Bangalore")</li><li>
+                Nasa Engineer</li><li>
+                Navy Reservist ("Simpson Tide")</li><li>
+                Oil-rig worker("Half-Decent Proposal")</li><li>
+                One Man Band ("Bart Gets Famous")</li><li>
+                Opera singer ("The Homer of Seville.")</li><li>
+                Ordained Minister ("There's Something About Marrying")</li><li>
+                Outsider Artist ("Mom and Pop Art")</li><li>
+                Owner</li><li>
+                Paparazzo ("Homerazzi").</li><li>
+                Performance artist -Getting hit with a cannonball ("Homerpalooza")</li><li>
+                Personal assistant</li><li>
+                Police officer/Police Chief ("Poppa's Got a Brand New Badge")</li><li>
+                Prank monkey for Mr. Burns ("Homer vs. Dignity")</li><li>
+                Public speaker ("HOM?")</li><li>
+                Quiz-Master (Kill the Alligator and Run)</li><li>
+                Roadie ("How I Spent My Strummer Vacation")</li><li>
+                Railroad Engineer (implied) ("Maximum Homerdrive")</li><li>
+                Rollercoaster Rebuilder ("Please Homer, Don't Hammer 'Em")</li><li>
+                Safety Salamander ("See Homer Run")</li><li>
+                Sailor in the Naval Reserve ("Simpson Tide")</li><li>
+                Sanitation Commissioner ("Trash of the Titans")</li><li>
+                Security Officer of SpringShield ("Poppa's Got a Brand New Badge")</li><li>
+                Sideshow freak ("Homerpalooza")</li><li>
+                Silhouette Model ("Midnight Towboy")</li><li>
+                Singer ("Homer's Barbershop Quartet")</li><li>
+                Smuggler</li><li>
+                Snowplow proprietor and driver ("Mr. Plow")</li><li>
+                Soccer referee ("Marge Gamer" and "You Don't Have to Live Like a Referee")</li><li>
+                Soldier ("G.I. (Annoyed Grunt)")</li><li>
+                Softball Player ("Homer at the Bat")</li><li>
+                Street musician ("Bart Gets Famous")</li><li>
+                Superhero ("Simple Simpson", "Treehouse of Horror XXII")</li><li>
+                Spokesperson/Walking Billboard, ("My Fair Laddy")</li><li>
+                Sprawl-Mart greeter ("Goo Goo Gai Pan")</li><li>
+                Talk Show Host ("Today, I Am a Clown")</li><li>
+                Teacher ("Secrets of a Successful Marriage")</li><li>
+                Television Producer</li><li>
+                Telemarketer ("Lisa's Date with Density")</li><li>
+                Tomacco Creator/Farmer/Salesman (see Farmer) ("E-I-E-I-D'oh")</li><li>
+                Tow Truck Driver ("Midnight Towboy")</li><li>
+                Town crier ("Lisa the Iconoclast")</li><li>
+                Traveling salesman of Simpson & Son Revitalizing Tonic ("Grampa vs. Sexual Inadequacy")</li><li>
+                Trucker ("Maximum Homerdrive")</li><li>
+                TV Show Host ("Politically Inept, with Homer Simpson")</li><li>
+                Union leader ("Last Exit to Springfield")</li><li>
+                Used Car salesman ("Diatribe of a Mad Housewife")</li><li>
+                Voice actor ("The Itchy & Scratchy & Poochie Show")</li><li>
+                Webmaster ("The Computer Wore Menace Shoes")</li><li>
+                Writer</li>
+                <ul>
+                    <li>Poppa's Got a Brand New Badge</li><li>
+                    Homer's Odyssey</li><li>
+                    Two Dozen and One Greyhounds</li><li>
+                    G.I. (Annoyed Grunt)</li><li>
+                    Donnie Fatso</li><li>
+                    Colonel Homer</li><li>
+                    A Star is Torn</li><li>
+                    Diatribe of a Mad Housewife</li><li>
+                    Marge's Son Poisoning</li><li>
+                    Deep Space Homer</li><li>
+                    I Married Marge </li>
+                </ul>
+            </ul>
+                    <blockquote>
+                        <p>
+                            "Trying is the first step towards failure."
+                        </p>
+                        <footer>Homer Jay Simpson</footer>
+                    </blockquote>
+                    <h3>
+                        <a href="http://simpsons.wikia.com/wiki/List_of_Homer's_jobs" target="_blank">  Read more / source  </a>
+                    </h3>
+                    </div>
+                </div>
+            </div>
+            <footer class="text-center">
+                <hr>
+                Written and coded by <a href="https://github.com/QGfcc/learning">QG</a>
+            </footer>
+        </div>
+    </body>
+</html>
+
