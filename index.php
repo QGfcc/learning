6cf174d64ab59dcc8343f700b6303a9fb8b0fe82
queni <queni@queni-PC>
2016-05-09 23:36:02 +0200

    Portfolio initial commit index.php redirect

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta http-equiv="refresh" content="0, portfolio.php">
+    <meta charset="utf-8"> <!-- Latest compiled and minified CSS -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!--<link href="testFixedBackground.css" rel="stylesheet" type="text/css"/>-->
+  </head>
+  <body>
+<!--      test
+      
+      <div class="cd-fixed-bg cd-bg-1">
+          qsdf
+      </div><br>
+      
+      <div class="cd-fixed-bg cd-bg-2">
+          qqdsfqsdfqsd
+      </div><br>
+      
+      <div class="cd-fixed-bg cd-bg-3">
+          qsdfqsdfqsdfqsdfqsd
+      </div><br>
+      
+      <div class="cd-fixed-bg cd-bg-4">
+          qdfqsdfqsdfqsdfqsdfqsdfqsdfqdfs
+      </div><br>
+      
+      -->
+      
+      
+  </body>
+</html>
